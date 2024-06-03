59d230f5362653aec1b3c9df5d4b68d20d30f0f3
Vlad Bondariev <151536127+VdBondarev@users.noreply.github.com>
2024-06-03 18:54:12 +0300

    added some config, docker support, some dependencies etc (#1)

--- src/main/java/com/bond/App.java
+++ src/main/java/com/bond/App.java
@@ -1,13 +1,10 @@
 package com.bond;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
