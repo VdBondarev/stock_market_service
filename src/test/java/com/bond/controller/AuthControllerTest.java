cc055f1e07b46eb71787ec0da1079a3f4cfc7c67
Vlad Bondarev <bondarev.21@icloud.com>
2024-06-05 17:44:56 +0300

    fully tested AuthController

--- src/test/java/com/bond/controller/AuthControllerTest.java
+++ src/test/java/com/bond/controller/AuthControllerTest.java
@@ -1,10 +1,14 @@
 package com.bond.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.bond.dto.user.UserLoginRequestDto;
+import com.bond.dto.user.UserLoginResponseDto;
 import com.bond.dto.user.UserRegistrationRequestDto;
 import com.bond.dto.user.UserResponseDto;
 import com.bond.holder.LinksHolder;
@@ -24,6 +28,8 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuthControllerTest extends LinksHolder {
     protected static MockMvc mockMvc;
+    private static final String JWT_REGEX =
+            "^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_.+/=]*$";
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -76,7 +82,7 @@ class AuthControllerTest extends LinksHolder {
             "Verify that registration endpoint works as expected when passing a registered email"
     )
     @Sql(
-            scripts = { ADD_USER_TO_DATABASE_FILE_PATH },
+            scripts = {INSERT_USER_TO_DATABASE_FILE_PATH},
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
     )
     @Sql(
@@ -106,6 +112,88 @@ class AuthControllerTest extends LinksHolder {
         String actualMessage = result.getResolvedException().getMessage();
 
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName(
+            "Verify that registration endpoint works as expected when passing non-valid params"
+    )
+    @Sql(
+            scripts = {
+                    REMOVE_ALL_USERS_FILE_PATH,
+                    INSERT_USER_TO_DATABASE_FILE_PATH
+            },
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    void register_NonValidParam_Failure() throws Exception {
+        UserRegistrationRequestDto requestDto = createUserRegistrationRequestDto(
+                "non-valid",
+                "non-valid",
+                "non-valid",
+                "1234",
+                "1234"
+        );
+
+        String content = objectMapper.writeValueAsString(requestDto);
+
+        // expecting that input validation will work and will not let a request go further
+        mockMvc.perform(
+                        post("/auth/registration")
+                                .content(content)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Sql(
+            scripts = {
+                    REMOVE_ALL_USERS_FILE_PATH,
+                    INSERT_USER_TO_DATABASE_FILE_PATH,
+                    REMOVE_ALL_USER_ROLES_FILE_PATH
+            },
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Sql(
+            scripts = {
+                    REMOVE_ALL_USERS_FILE_PATH,
+                    REMOVE_ALL_USER_ROLES_FILE_PATH
+            },
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
+    @Test
+    @DisplayName("Verify that login() endpoint works as expected")
+    void login_ValidInput_Success() throws Exception {
+        UserLoginRequestDto requestDto = new UserLoginRequestDto();
+        requestDto.setPassword("1234567890");
+        requestDto.setEmail("admin@example.com");
+
+        String content = objectMapper.writeValueAsString(requestDto);
+
+        MvcResult result = mockMvc.perform(get("/auth/login")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        UserLoginResponseDto responseDto = objectMapper.readValue(
+                result.getResponse().getContentAsString(), UserLoginResponseDto.class
+        );
+
+        assertTrue(responseDto.getToken().matches(JWT_REGEX));
+    }
+
+    private UserRegistrationRequestDto createUserRegistrationRequestDto(
+            String firstName,
+            String lastName,
+            String email,
+            String password,
+            String repeatPassword
+    ) {
+        return new UserRegistrationRequestDto()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setRepeatPassword(repeatPassword)
+                .setPassword(password);
     }
 
     private UserResponseDto createUserResponseDto(Long id, UserRegistrationRequestDto requestDto) {
--- src/test/java/com/bond/holder/LinksHolder.java
+++ src/test/java/com/bond/holder/LinksHolder.java
@@ -3,6 +3,10 @@ package com.bond.holder;
 public abstract class LinksHolder {
     protected static final String REMOVE_ALL_USERS_FILE_PATH =
             "classpath:database/remove-all-users-from-database.sql";
-    protected static final String ADD_USER_TO_DATABASE_FILE_PATH =
+    protected static final String INSERT_USER_TO_DATABASE_FILE_PATH =
             "classpath:database/insert-user-to-database.sql";
+    protected static final String REMOVE_ALL_USER_ROLES_FILE_PATH =
+            "classpath:database/delete-all-users_roles-from-database.sql";
+    protected static final String INSERT_ADMIN_ROLES_FILE_PATH =
+            "classpath:database/insert-admin-roles-to-user_roles-table.sql";
 }
